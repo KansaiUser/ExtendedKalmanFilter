c4940287a697e2ee9465c91b8b6cee72e45469be
David Aliaga <aliaga@sensetime.jp>
2020-11-19 10:43:25 +0900

    implemented RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,6 +14,38 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+   int n= estimations.size();
+   float sum=0.0;
+
+   VectorXd rmse(4);
+   rmse<<0,0,0,0;
+
+   //First some checks
+   if(estimations.size()==0)
+    {    std::cout<<"Error, estimation size is 0"<<std::endl;
+      return rmse;
+    }
+   if(estimations.size()!=ground_truth.size())
+  {
+      std::cout<<"Error, estimation and grund truth size is different"<<std::endl;
+      return rmse;
+  }
+
+   for(int i= 0 ; i< n; i++)
+   {
+      VectorXd difference= estimations[i]-ground_truth[i];
+      
+      difference=difference.array()*difference.array();   
+
+      rmse+= difference;
+
+   }
+
+   rmse*=(1/n);
+   rmse=rmse.array().sqrt();
+   
+   return rmse;
+
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
